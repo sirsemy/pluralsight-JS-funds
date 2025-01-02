faaaccc0adc66b555bcb3d910ad5080a0629658d
sirsemy <hajdu1szabolcs@gmail.com>
2025-01-02 20:54:32 +0100

    Add boolean practice file

---
+++ booleans.js
@@ -0,0 +1,9 @@
+let trueValue = true;
+console.log(trueValue);
+let falseValue = false;
+console.log(falseValue);
+
+let notTrue = !true;
+console.log(notTrue);
+let notFalse = !false;
+console.log(notFalse);
