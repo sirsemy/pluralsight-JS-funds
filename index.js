8c92bbe54570e536627e96791a1f949989da3c2c
sirsemy <hajdu1szabolcs@gmail.com>
2025-01-03 13:43:43 +0100

    Add some running settings

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import date from 'date.js';
+/* import date from 'date.js';
 
 // This example takes 2 seconds to run
 const start = date('now');
@@ -11,17 +11,9 @@ setTimeout(() => {
 
     console.log(`seconds elapsed = ${Math.floor(millis / 1000)}`);
   // Expected output: "seconds elapsed = 2"
-}, 2000);
-
-/* // This example takes 2 seconds to run
-const start = Date.now();
-
-console.log('starting timer...');
-// Expected output: "starting timer..."
-
-setTimeout(() => {
-  const millis = Date.now() - start;
-
-  console.log(`seconds elapsed = ${Math.floor(millis / 1000)}`);
-  // Expected output: "seconds elapsed = 2"
 }, 2000); */
+
+let firstName = "David";
+let lastName = "Tucker";
+
+console.log(`Hello ${firstName} ${lastName}`);
