2fcd1c0033245857fb0900571dd3bd266fa05379
sirsemy <hajdu1szabolcs@gmail.com>
2025-01-17 12:42:38 +0100

    Add math.js practice file

---
+++ conditionals/math.js
@@ -0,0 +1,36 @@
+let val1 = 3 + 2;
+console.log("val1: ", val1);
+let val2 = 2 + -1;
+console.log("val2: ", val2);
+
+let val3 = 3 - 1;
+console.log("val3: ", val3);
+let val4 = 1 - -4;
+console.log("val4: ", val4);
+
+let val5 = 3 / 1;
+console.log("val5: ", val5);
+let val6 = 21 / 9;
+console.log("val6: ", val6);
+
+let val7 = 3 * 2;
+console.log("val7: ", val7);
+let val8 = 3 * 3;
+console.log("val8: ", val8);
+
+let val9 = 9 % 2;
+console.log("val9: ", val9);
+let val10 = -10 % 7;
+console.log("val10: ", val10);
+
+let val11 = 3 ** 2;
+console.log("val11: ", val11);
+let val12 = 2 ** -1;
+console.log("val12: ", val12);
+
+let val13 = 1;
+++val13;
+console.log("val13: ", val13);
+let val14 = 10;
+--val14;
+console.log("val14: ", val14);
