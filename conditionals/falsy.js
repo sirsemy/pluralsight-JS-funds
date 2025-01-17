89f95a279c01de0a966ee11f98f96444e6956b9d
sirsemy <hajdu1szabolcs@gmail.com>
2025-01-17 13:36:58 +0100

    Add falsy practice examples file

---
+++ conditionals/falsy.js
@@ -0,0 +1,27 @@
+// simply function to log out truthyness
+const isTruthy = (name, exp) => {
+    console.log(`${name}: ${Boolean(exp)}`);
+}
+
+isTruthy("val1", 0);
+isTruthy("val2", 1);
+isTruthy("val3", -1);
+isTruthy("val4", NaN);
+isTruthy("val5", 0n);
+
+// Boolean, Null and undefined
+isTruthy("val6", undefined);
+isTruthy("val7", null);
+isTruthy("val8", false);
+
+// Strings
+isTruthy("val9", "");
+isTruthy("val10", "Hi");
+isTruthy("val11", "false");
+
+// Objects
+isTruthy("val12", {});
+
+// Undefined variables
+let var1;
+isTruthy("val13", var1);
